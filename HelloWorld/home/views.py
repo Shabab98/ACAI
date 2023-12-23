64ec3246bc33b122e5e3a6846e6ec08b49827778
Shabab98 <154035323+Shabab98@users.noreply.github.com>
2023-12-23 16:57:11 -0500

    Updated with Templates

--- HelloWorld/home/views.py
+++ HelloWorld/home/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-# home/views.py
-# home/views.py
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from home.models import Library, UserProfile
@@ -14,9 +10,19 @@ class LibraryListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Library.objects.all()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Library List'
+        return context
+
 class UserListView(LoginRequiredMixin, ListView):
     model = UserProfile
     template_name = 'home/user_list.html'
 
     def get_queryset(self):
         return UserProfile.objects.filter(role='user')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'User List'
+        return context
